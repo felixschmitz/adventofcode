ee18589619eeff2efbe12626d642513cf814832f
Felix Schmitz <felixschmitz57@gmail.com>
2022-12-08 12:07:03 +0100

    tested solution for day08

---
+++ day08/test_solution.py
@@ -0,0 +1,12 @@
+from solution import first_task, second_task
+
+def test_first_task():
+    sum_ = first_task('sample_input.txt')
+    assert type(sum_) == int
+    assert sum_ == 21
+
+
+def test_second_task():
+    max_ = first_task('sample_input.txt')
+    assert type(max_) == int
+    assert max_ == 8
