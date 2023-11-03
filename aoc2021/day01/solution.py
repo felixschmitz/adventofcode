ce698e594fe6ce7e198692b5790d85ea8dd86065
Felix Schmitz <felixschmitz57@gmail.com>
2023-11-03 16:36:36 +0100

    mix up between day01 and day02

--- aoc2021/day01/solution.py
+++ aoc2021/day01/solution.py
@@ -11,6 +11,14 @@ def depth_transformer(data: list) -> list:
     return [data[i] - data[i - 1] > 0 for i in range(1, len(data))]
 
 
+def sliding_window(data: list, window_size: int) -> list:
+    return list(
+        map(
+            sum, [data[i : i + window_size] for i in range(len(data) - window_size + 1)]
+        )
+    )
+
+
 def first_task(file: str):
     content = data_loader(file)
     data = data_parser(content)
@@ -18,5 +26,16 @@ def first_task(file: str):
     return sum(data_increasing)
 
 
+def second_task(file: str):
+    content = data_loader(file)
+    data = data_parser(content)
+    data_windows = sliding_window(data, 3)
+    data_increasing = depth_transformer(data_windows)
+    return sum(data_increasing)
+
+
 increase_counter = first_task("input.txt")
 print("First task:", increase_counter)
+
+increase_window_counter = second_task("input.txt")
+print("Second task:", increase_window_counter)
