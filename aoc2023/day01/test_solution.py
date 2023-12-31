b7a4bbedca16498c8a6d01129344b2332b5173ee
Felix Schmitz <felixschmitz57@gmail.com>
2023-12-31 14:19:40 +0100

    Amendment to testing the solution of day01

--- aoc2023/day01/test_solution.py
+++ aoc2023/day01/test_solution.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 from .solution import first_task, second_task
 
+SAMPLE_PATH = Path(__file__).parent / "sample_input.txt"
+
 
 def test_first_task():
-    sample_path = Path(__file__).parent / "sample_input.txt"
-    solution = first_task(sample_path)
+    solution = first_task(SAMPLE_PATH)
     assert solution == 142
 
 
 def test_task2():
-    sample_path = Path(__file__).parent / "sample_input_2.txt"
-    solution = second_task(sample_path)
+    solution = second_task(SAMPLE_PATH)
     assert solution == 281
